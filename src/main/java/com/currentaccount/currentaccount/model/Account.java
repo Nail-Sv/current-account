ef2ef1ea1ec702f9d019ca58a9041cb8ef7fdcfc
nail-sv <nail.sv86@gmail.com>
2024-05-30 12:49:29 +0200

    add Account, Customer, Transaction Entity

---
+++ src/main/java/com/currentaccount/currentaccount/model/Account.java
@@ -0,0 +1,55 @@
+package com.currentaccount.currentaccount.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.io.Serial;
+import java.util.List;
+import java.util.UUID;
+
+@Entity(name = "account")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Account {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The unique identifier of the customer. This is a UUID because we don't want to expose number of customers.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "account_id")
+    private UUID id;
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "account")
+    private List<Transaction> transactions;
+
+    @NotBlank
+    private double balance;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Account other = (Account) obj;
+        return id != null && id.equals(other.getId());
+    }
+}
