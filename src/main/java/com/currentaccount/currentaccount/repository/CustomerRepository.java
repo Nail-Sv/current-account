46d1e640356aff3fb33befa5c46090a7525760e9
nail-sv <nail.sv86@gmail.com>
2024-05-30 14:34:14 +0200

    add repository

---
+++ src/main/java/com/currentaccount/currentaccount/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.currentaccount.currentaccount.repository;
+
+import com.currentaccount.currentaccount.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+}
