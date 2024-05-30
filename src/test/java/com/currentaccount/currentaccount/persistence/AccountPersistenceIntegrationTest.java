db91b6aac405d9673b669e93e84e7b7444f7cd83
nail-sv <nail.sv86@gmail.com>
2024-05-30 18:37:09 +0200

    persistence tests refactoring

--- src/test/java/com/currentaccount/currentaccount/persistence/AccountPersistenceIntegrationTest.java
+++ src/test/java/com/currentaccount/currentaccount/persistence/AccountPersistenceIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.currentaccount.currentaccount.service.persistence;
+package com.currentaccount.currentaccount.persistence;
 
 import com.currentaccount.currentaccount.model.Account;
 import com.currentaccount.currentaccount.model.Customer;
